39afe558863d32b524d6cffc95588e25c4a60d47
OStefan2001 <olarustefi1234@gmail.com>
2026-07-01 11:56:36 +0300

    meta updates

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -6,8 +6,8 @@ $app = [
     'baseUrl' => $baseUrl,
     'name' => 'Dotkernel Light | PSR-15 compliant application',
     'meta' => [
-        'title'       => 'Dotkernel Light',
-        'description' => 'Dotkernel is a Headless Platformfor building modern web applications				
+        'title'       => 'Dotkernel | Headless Platform for modern web application',
+        'description' => 'Dotkernel is a Headless Platform for building modern web applications				
 					      Dotkernel is a collection of applications (skeletons) that use a middleware-first architecture 
 					      built on top of the Mezzio microframework using Laminas components. The goal is to provide a 
 					      pre-configured environment for app',
