0c6531018660bd1aa4b84202d71e571f9d933b0b
OStefan2001 <olarustefi1234@gmail.com>
2026-07-02 15:07:48 +0300

    unittest: update abstractentity

--- src/App/src/Entity/AbstractEntity.php
+++ src/App/src/Entity/AbstractEntity.php
@@ -4,80 +4,31 @@ declare(strict_types=1);
 
 namespace Light\App\Entity;
 
-use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 use Laminas\Stdlib\ArraySerializableInterface;
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
 
 use function is_array;
 use function method_exists;
 use function ucfirst;
 
 #[ORM\MappedSuperclass]
-abstract class AbstractEntity implements ArraySerializableInterface
+abstract class AbstractEntity implements ArraySerializableInterface, EntityInterface
 {
-    #[ORM\Id]
-    #[ORM\Column(name: 'id', type: 'uuid', unique: true, nullable: false)]
-    protected UuidInterface $id;
-
-    #[ORM\Column(name: 'created', type: 'datetime_immutable', nullable: false)]
-    protected DateTimeImmutable $created;
-
-    #[ORM\Column(name: 'updated', type: 'datetime_immutable', nullable: true)]
-    protected ?DateTimeImmutable $updated = null;
-
     public function __construct()
     {
-        $this->id = Uuid::uuid7();
+        $this->initId();
     }
 
-    public function getId(): UuidInterface
+    protected function initId(): void
     {
-        return $this->id;
     }
 
-    public function setId(UuidInterface $id): static
+    /**
+     * Override this method in soft-deletable entities
+     */
+    public function isDeleted(): bool
     {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getCreated(): ?DateTimeImmutable
-    {
-        return $this->created;
-    }
-
-    public function getCreatedFormatted(string $dateFormat = 'Y-m-d H:i:s'): string
-    {
-        return $this->created->format($dateFormat);
-    }
-
-    public function getUpdated(): ?DateTimeImmutable
-    {
-        return $this->updated;
-    }
-
-    public function getUpdatedFormatted(string $dateFormat = 'Y-m-d H:i:s'): ?string
-    {
-        if ($this->updated instanceof DateTimeImmutable) {
-            return $this->updated->format($dateFormat);
-        }
-
-        return null;
-    }
-
-    #[ORM\PrePersist]
-    public function created(): void
-    {
-        $this->created = new DateTimeImmutable();
-    }
-
-    #[ORM\PreUpdate]
-    public function touch(): void
-    {
-        $this->updated = new DateTimeImmutable();
+        return false;
     }
 
     /**
