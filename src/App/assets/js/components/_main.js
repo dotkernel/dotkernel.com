75fae43237569d2cc8a340c75897f95239b5329c
OStefan2001 <olarustefi1234@gmail.com>
2026-07-14 20:56:31 +0300

    Landing page v2

--- src/App/assets/js/components/_main.js
+++ src/App/assets/js/components/_main.js
@@ -1,4 +1,14 @@
 import $ from 'jquery';
 
-$(document).ready(function () {
-});
+function applyTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+}
+
+function toggleTheme() {
+    var html = document.documentElement;
+    var isLight = html.getAttribute('data-theme') === 'light';
+    var next = isLight ? 'dark' : 'light';
+    applyTheme(next);
+    localStorage.setItem('theme', next);
+}
+window.toggleTheme = toggleTheme;
